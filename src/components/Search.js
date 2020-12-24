06e90a50435de9e3b3d5d353c360f3fc2d55fa3c
Aurelien Morvan <aurelien.morvan@smile.fr>
2020-12-24 01:08:53 +0100

    add (Style) : Utilisation Stylesheet

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,15 +1,30 @@
 import React from 'react';
-import {Button, TextInput, View} from "react-native";
+import {Button, TextInput, View, StyleSheet} from "react-native";
 
 class Search extends React.Component {
     render() {
         return(
             <View>
-                <TextInput placeholder='Titre du film' onChangetext={text => console.warn(text)} />
+                <TextInput
+                    placeholder='Titre du film'
+                    onChangetext={text => console.warn(text)}
+                    style={styles.textInput}
+                />
                 <Button title="Rechercher" onPress={() => {}} />
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    textInput: {
+        marginLeft: 5,
+        marginRight: 5,
+        height: 50,
+        borderColor: '#000000',
+        borderWidth: 1,
+        paddingLeft: 5
+    }
+})
 
 export default Search
